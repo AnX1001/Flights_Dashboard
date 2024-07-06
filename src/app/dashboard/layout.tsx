2966d976d34013490df2a59118b129e9623dbbd0
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-07-06 18:03:35 +0200

    feat: align dashboard in the center

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
+import styles from './layout.module.css';
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className={styles.container}>
       <main>{children}</main>
     </div>
   );
