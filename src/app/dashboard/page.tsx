a55f48804168359c6a0a9fee3c22bd7316baf978
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-07-07 14:03:37 +0200

    refactor: create a generic fetch function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,40 +5,23 @@ import UserInfo from './components/UserInfo';
 import styles from './page.module.css';
 
 export default async function DashboardPage() {
-  async function getWeather() {
-    const url = 'http://localhost:3000/api/weather';
+  async function fetchData(url: string) {
     try {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
+
       const json = await response.json();
       return { data: json, error: null };
     } catch (error) {
       const errorMessage = error instanceof Error ? error.message : String(error);
-
       return { data: null, error: errorMessage };
     }
   }
 
-  async function getFlights() {
-    const url = 'http://localhost:3000/api/flights';
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      const json = await response.json();
-      return { data: json, error: null };
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : String(error);
-
-      return { data: null, error: errorMessage };
-    }
-  }
-
-  const weather = await getWeather();
-  const flights = await getFlights();
+  const weather = await fetchData('http://localhost:3000/api/weather');
+  const flights = await fetchData('http://localhost:3000/api/flights');
 
   return (
     <div className={styles.container}>
