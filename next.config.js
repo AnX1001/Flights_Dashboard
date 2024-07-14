b61090995b091698e2c49de3e16deef235e2671f
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-07-14 10:22:28 +0200

    chore(csp): add directive to allow YouTube iframe

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+ 
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data:;
-    font-src 'self'  
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
+ 
+    font-src 'self'  https://fonts.gstatic.com;  
+   
+    child-src https://www.youtube.com;
+ 
 `;
 
 module.exports = {
