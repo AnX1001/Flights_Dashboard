49e11f483ff72c3aa6023a6ab8c14cc9d658d0a3
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-07-15 13:52:15 +0200

    feature: add digital clock updates every second

--- src/app/dashboard/components/UserInfo.tsx
+++ src/app/dashboard/components/UserInfo.tsx
@@ -1,25 +1,49 @@
+'use client';
+
 import WidgetCard from './WidgetCard.tsx';
+import { useEffect, useState } from 'react';
 
 export default function UserInfo() {
-  const now = new Date().toISOString();
+  const [now, setNow] = useState(new Date());
 
-  const currentTime = now.split('T')[1].split('.')[0];
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
 
-  const getcurrentHour = new Date().getHours();
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const currentTime = now.toLocaleTimeString('nb-no', {
+    timeZone: 'Europe/Oslo',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  const getCurrentHour = new Date().toLocaleString('nb-no', {
+    timeZone: 'Europe/Oslo',
+    hour: '2-digit',
+    hour12: false,
+  });
 
   const timeGreeting = () => {
-    if (getcurrentHour > 12) {
-      return 'Good afternoon';
-    } else if (getcurrentHour > 16) {
-      return 'Good evening';
-    } else {
+    const hour = parseInt(getCurrentHour, 10);
+    if (hour < 12) {
       return 'Good morning';
+    } else if (hour < 18) {
+      return 'Good afternoon';
+    } else {
+      return 'Good evening';
     }
   };
 
   return (
     <>
-      <WidgetCard heading={`${timeGreeting()} An!`} info={`Current time: ${currentTime}`} />
+      <WidgetCard
+        heading={`${timeGreeting()} An!`}
+        info={`Local time Oslo/Norway: ${currentTime}`}
+      />
     </>
   );
 }
