4f080a0524653485be8f0026eaba09f71c71adcf
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-09-06 23:32:22 +0200

    chore: run lint-staged with NextJS config

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '*.{ts,tsx}': [buildEslintCommand],
+  '*.test.tsx': ['npm run test'],
+  '**/*': 'prettier --write --ignore-unknown',
+};
