2172185be0f1cf9b3524b0f4bfd12fb094631d16
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-09-13 23:50:48 +0200

    feat: add session duration

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,15 +1,18 @@
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
 
-import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
-
-
-export const {auth, handlers:{GET, POST}} = NextAuth({
-    providers:[
-        GitHub({
-            clientId:process.env.GITHUB_CLIENT_ID,
-            clientSecret:process.env.GITHUB_CLIENT_SECRET
-        }),
-        
-    ],
-    secret:process.env.AUTH_SECRET
-})
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    maxAge: 5 * 1, // 5 seconds before session expires
+  },
+  secret: process.env.AUTH_SECRET,
+});
